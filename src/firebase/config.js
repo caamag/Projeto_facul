8c6518e682e25902b779384d01e3e2c130212714
Caio Lopes <caiolopesfv@gmail.com>
2024-09-27 22:17:46 -0300

    adicionando configurações firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCBg-RbIQRn8biT19kzPIpMHse1rOm3E9E",
+    authDomain: "projeto-facul-750af.firebaseapp.com",
+    projectId: "projeto-facul-750af",
+    storageBucket: "projeto-facul-750af.appspot.com",
+    messagingSenderId: "886350017777",
+    appId: "1:886350017777:web:e697673840b8e82c236550",
+    measurementId: "G-WSML58GW76"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+export { db };
