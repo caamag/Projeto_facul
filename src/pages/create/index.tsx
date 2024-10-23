c6a5dfe87f5721b39e6b9c666c7c77f22c196730
BCR-0632 <caiolopesfv@gmail.com>
2024-10-23 20:50:02 -0300

    Enviando solicitações

--- src/pages/create/index.tsx
+++ src/pages/create/index.tsx
@@ -1,9 +1,42 @@
 import * as Css from './style';
+import { useState } from 'react';
 
 const Create = () => {
+
+    const [title, setTitle] = useState<string>('')
+    const [ingredient, setIngredient] = useState<string>('')
+    const [method, setMethod] = useState<string>('')
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+    }
+
     return (
         <Css.CreateContainer>
+            <h1>Nos envie a sua própria receita</h1>
+            <h2>Ela será avaliada por um especialista e então postada em nosso feed.</h2><br />
 
+            <Css.createForm onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder='Título da receita:'
+                    value={title}
+                    onChange={(e) => { setTitle(e.target.value) }}
+                />
+
+                <textarea
+                    placeholder='Informe os ingredientes:'
+                    value={ingredient}
+                    onChange={(e) => { setIngredient(e.target.value) }}
+                />
+
+                <textarea
+                    placeholder='Informe o modo de preparo da receita:'
+                    value={method}
+                    onChange={(e) => { setMethod(e.target.value) }}
+                />
+                <button type='submit'>Enviar para especialista</button>
+            </Css.createForm>
         </Css.CreateContainer>
     )
 }
--- src/pages/create/style.ts
+++ src/pages/create/style.ts
@@ -1,5 +1,56 @@
 import styled from "styled-components";
 
 export const CreateContainer = styled.div`
-    
+    width: 100%;
+
+    h1{
+        text-align: center;
+    }
+
+    h2{
+        text-align: center;
+        font-weight: 400;
+    }
+`
+
+export const createForm = styled.form`
+    width: 60%;
+    margin: 0 auto;
+    padding: 15px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    input {
+        width: 100%;
+        padding: 15px;
+        border: solid 1px gray;
+        font-size: 16px;
+        margin-bottom: 20px;
+    }
+
+    textarea {
+        width: 100%;
+        padding: 15px;
+        border: 1px solid gray;
+        font-size: 16px;
+        min-height: 100px;
+        resize: none;
+        margin-bottom: 20px;
+    }
+
+    button{
+        width: 100%;
+        padding: 15px 20px;
+        background-color: white;
+        font-size: 16px;
+        transition: 150ms;
+        border: 1px solid rgb(40,40,40);
+        color: rgb(40,40,40);
+
+        &:hover{
+            background-color: rgb(40,40,40);
+            color: white;
+        }
+    }
 `
